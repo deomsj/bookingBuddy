ff51586e8a989a1f8cf5bea278cf8f5bf8ac4177
Max Berger <maxamillianberger@gmail.com>
2017-04-06 16:21:50 -0700

    Create basic server and client

--- server/server.js
+++ server/server.js
@@ -2,3 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(port, function() {
+  console.log('listening on', port);
+});
