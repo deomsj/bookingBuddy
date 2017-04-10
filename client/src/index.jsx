cefb74f21af7e6500d50dff4ab179d1e701e487e
Lou Kaileh <lkaileh@gmail.com>
2017-04-10 14:52:38 -0700

    setup of webpack

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,15 @@
+//import _ from 'lodash';
+function component () {
+  var element = document.createElement('div');
+
+  /* lodash is required for the next line to work */
+  //element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
 var TripRoomNav = function() {
   return (
     <div>
