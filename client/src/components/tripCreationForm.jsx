629084de3540135bc923beceba679f1c361e608d
Nate Nault <nate@nathannault.com>
2017-04-12 07:26:25 +0800

    Update component names and add basic markup for testing

---
+++ client/src/components/tripCreationForm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TripCreationForm extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Trip Creation Form</h2>
+      </div>
+    );
+  }
+}
+
+export default TripCreationForm;
---
+++ client/src/components/tripPreferencesForm.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const TripPreferencesForm = () => (
+  <h2>Trip Preferences Form</h2>
+);
+
+export default TripPreferencesForm;
