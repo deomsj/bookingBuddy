c57d5b44955b7ce76ede554cf5f0be09123a60f2
Preston Moore <caroham29@gmail.com>
2017-04-13 12:36:40 -0700

    Yessir

--- client/src/components/authentication/signUp.jsx
+++ client/src/components/authentication/signUp.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const Signup = () => (
-  <h2>Signup</h2>
-);
+
+
+class Signup extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      membersInvited: [],
+      tripName: '',
+      tripSummary: ''
+    };
+  }
+
+  changeTripName(e) {
+    this.setState({
+      tripName: e.target.value,
+    });
+  }
+
+  changeTripSummary(e) {
+    this.setState({
+      tripSummary: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Signup</h1>
+        <p>Trip Name:</p>
+        <input onChange={this.changeTripName} value={this.state.tripName} /> <br />
+        <p>Trip Description:</p>
+        <textarea onChange={this.changeTripSummary} defaultValue={this.state.tripSummary}/>
+      </div>
+    );
+  }
+
+}
 
 export default Signup;
