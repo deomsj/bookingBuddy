4afea1c017e87f2c83dff6b13a081fee38306eb7
Max Berger <maxamillianberger@gmail.com>
2017-04-14 08:31:14 -0700

    Style trip room page

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  hotWireApiKey: '93w4ahrxdpy96pj6mxnaxn2t',
+  skyScannerApiKey: 'bo627419382875737954157979434977'
+};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ var routes = require('./routes');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
+var Hotwire = require('hotwire');
+
+var hotwire = new Hotwire('93w4ahrxdpy96pj6mxnaxn2t');
+
+// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/test6';
+// var client = new pg.Client(connectionString);
+
+// client.connect(function (err) {
+//   if (err) throw err;
+// });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, '../client')));
