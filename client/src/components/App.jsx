8b18ff54605f43c0117492b881c3401142ce35e4
Nate Nault <nate@nathannault.com>
2017-04-19 09:53:33 +0800

    Refactor routes to include authentication

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   NavLink
 } from 'react-router-dom';
+import {
+  AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN
+} from '../../../env.config.js';
 
 // Components
 import Profile from './Profile.jsx';
@@ -13,54 +17,118 @@ import TripRoom from './tripRoom/TripRoom.jsx';
 import TripCreationForm from './TripCreationForm.jsx';
 import TripPreferencesForm from './TripPreferencesForm.jsx';
 import PostRegistration from './authentication/PostRegistration.jsx';
+
 // Auth
 import Signin from './authentication/Signin.jsx';
 import Signup from './authentication/Signup.jsx';
+import AuthService from '../auth/AuthService';
+
+const LoginStatusDisplay = ({isloggedIn, logInOrOut}) => (
+  <NavLink onClick={logInOrOut} className='loginStatusDisplay'>
+    {isloggedIn ? 'Log Out' : 'Log In'}
+  </NavLink>
+);
 
 // Navigation Menu
-const Links = () => (
+const Links = ({isloggedIn, logInOrOut}) => (
   <nav className="green lighten-1" role="navigation">
-    <div className="nav-wrapper container"><NavLink id="logo-container" exact to="/" className="brand-logo">Booking Buddy</NavLink>
+    <div className="nav-wrapper container">
+      <NavLink id="logo-container" exact to="/" className="brand-logo">Booking Buddy</NavLink>
       <ul className="right hide-on-med-and-down">
-        <li><NavLink to="/profile">My Profile</NavLink></li>
-        <li><NavLink to="/signin">Sign In</NavLink></li>
-        <li><NavLink to="/signup">Register</NavLink></li>
+        {
+          isloggedIn ?
+          <li><NavLink to="/profile">My Profile</NavLink></li> :
+          <li><NavLink exact to="/">Home</NavLink></li>
+        }
+        <li>
+          <LoginStatusDisplay
+            isloggedIn={isloggedIn}
+            logInOrOut={logInOrOut}
+          />
+        </li>
         <li><NavLink to="/start-planning">Start Planning</NavLink></li>
-        <li><NavLink to="/signout">Sign Out</NavLink></li>
         <li><NavLink to="/trip-room/:tripId">TripRoom</NavLink></li>
-        <li><NavLink to="/postRegistration"> PostRegistration</NavLink></li>
+        <li><NavLink to="/postRegistration">PostRegistration</NavLink></li>
       </ul>
 
       <ul id="nav-mobile" className="side-nav">
-        <li><NavLink exact to="/">Home</NavLink></li>
-        <li><NavLink to="/profile">My Profile</NavLink></li>
-        <li><NavLink to="/signin">Sign In</NavLink></li>
-        <li><NavLink to="/signup">Register</NavLink></li>
-        <li><NavLink to="/postRegistration"> PostRegistration</NavLink></li>
+        {
+          isloggedIn ?
+          <li><NavLink to="/profile">My Profile</NavLink></li> :
+          <li><NavLink exact to="/">Home</NavLink></li>
+        }
+        <li>
+          <LoginStatusDisplay
+            isloggedIn={isloggedIn}
+            logInOrOut={logInOrOut}
+          />
+        </li>
+        <li><NavLink to="/postRegistration">PostRegistration</NavLink></li>
         <li><NavLink to="/start-planning">Start Planning</NavLink></li>
-        <li><NavLink to="/signout">Sign Out</NavLink></li>
-        <li><NavLink to="/trip-room/:tripId">TripRoom(Testing)</NavLink></li>
+        <li><NavLink to="/trip-room/:tripId">TripRoom</NavLink></li>
       </ul>
       <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
     </div>
   </nav>
 );
 
-// Routes
-const App = ({userInfo}) => (
-  <Router>
-    <div>
-      <Links />
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/signin" component={Signin} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/start-planning" component={StartPlanning} />
-      <Route path="/profile" component={() => <Profile userInfo={userInfo}/> }/>
-      <Route path="/postRegistration" component={PostRegistration}/>
-      <Route path="/trip-room/:tripId" component={TripRoom} />
-    </div>
-  </Router>
-);
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.logInOrOut = this.logInOrOut.bind(this);
+    this.state = {
+      isloggedIn: false,
+      profile: {}
+    };
+
+    this.auth = new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+
+    this.auth.on('profile_updated', (newProfile) => {
+      this.setState({
+        profile: newProfile,
+        isloggedIn: true
+      });
+    });
+  }
+
+  logInOrOut() {
+    if (!this.state.isloggedIn) {
+      this.auth.login();
+    } else {
+      this.auth.logout();
+      this.setState({
+        profile: {},
+        isloggedIn: false
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Links
+            isloggedIn={this.state.isloggedIn}
+            logInOrOut={this.logInOrOut}
+          />
+          <Route
+            exact path="/"
+            render={() =>
+            !this.state.isLoggedIn ?
+            <LandingPage /> :
+            <Redirect to='/profile' />
+          } />
+          <Route
+            path="/profile" render={() => <Profile userInfo={this.state.profile}/> }/>
+          <Route path="/start-planning" component={StartPlanning} />
+          <Route path="/postRegistration" component={PostRegistration}/>
+          <Route path="/trip-room/:tripId" component={TripRoom} />
+        </div>
+      </Router>
+    )
+  }
+};
 
 // Export module
 export default App;
