c4dcfa7d00bde91902782eb7b9d2c6a6003038f8
Jesse DeOms <jesse.deoms@gmail.com>
2017-04-20 06:36:12 -0700

    Add trip room components

---
+++ client/src/components/tripRoom/tripRoomComponents/TripRoomComponents.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import TripRecomendationsCards from './newRecomendations/tripRecomendationsCards.jsx';
+import GroupPreferencesBar from './groupPreferences/groupPreferencesBar.jsx';
+import TripBookmarksList from './bookmarksView/tripBookmarksList.jsx';
+
+class TripRoomComponents extends Component {
+
+  constructor (props) {
+    super(props);
+    this.setLocation = this.setLocation.bind(this);
+    this.addBookmark = this.addBookmark.bind(this);
+    this.updateBookmarkVote = this.updateBookmarkVote.bind(this);
+    this.state = {
+      priceRange: props.tripData.priceRange,
+      dateRange: props.tripData.dateRange,
+      locations: props.tripData.locations,
+      selectedLocation: '',
+      url:'',
+      bookmarkedTrips: props.tripData.bookmarkedTrips.slice()
+    };
+  }
+
+  setLocation(selection) {
+    this.setState({
+      selectedLocation: selection
+    });
+  }
+
+  addBookmark(newBookmark) {
+    newBookmark['buddyVotes'] = this.props.tripData.buddyList.map((buddyName) => ({
+      buddyName: buddyName,
+      buddyVote: 0
+    }));
+    newBookmark['bookmarkId'] = this.state.bookmarkedTrips.length;
+
+    this.setState({
+      bookmarkedTrips: this.state.bookmarkedTrips.concat(newBookmark)
+    });
+  }
+
+  updateBookmarkVote(bookmarkId, buddyName, newVote) {
+
+    var updatedBookmarks = this.state.bookmarkedTrips.map((bookmark) => {
+      if (bookmark.bookmarkId === bookmarkId) {
+        bookmark.buddyVotes.forEach( (buddyVoteObj) => {
+          if(buddyVoteObj.buddyName === buddyName) {
+            buddyVoteObj.buddyVote = newVote;
+          }
+        });
+      }
+      return bookmark;
+    });
+
+    this.setState({
+      bookmarkedTrips: updatedBookmarks
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <h1 className="orange-text darken-2">{this.props.tripData.tripName}</h1>
+        <GroupPreferencesBar
+          priceRange={this.state.priceRange}
+          dateRange={this.state.dateRange}
+          locations={this.state.locations}
+          setLocation={this.setLocation}
+        />
+        <TripRecomendationsCards
+          hotelRecomendations={this.props.hotelRecomendations}
+          addBookmark={this.addBookmark}
+        />
+        <TripBookmarksList
+          bookmarkedTrips={this.state.bookmarkedTrips}
+          updateBookmarkVote={this.updateBookmarkVote}
+        />
+      </div>
+    );
+  }
+};
+
+export default TripRoomComponents;
---
+++ client/src/components/tripRoom/tripRoomComponents/groupPreferences/groupPreferencesBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import DropDownFilter from './dropDownFilter.jsx';
+/////////////////////////
+// Group Preferences Bar
+/////////////////////////
+
+var GroupPreferencesBar = ({priceRange, dateRange, locations, setLocation}) => (
+  <div>
+    <div className="row">
+      <div className="col s3">
+        <span><i className="material-icons green-text">credit_card</i>{ priceRange }</span><br />
+      </div>
+      <div className="col s3">
+        <span><i className="material-icons green-text">today</i>{dateRange}</span><br /><br />
+      </div>
+    </div>
+    <span><strong>Based on your group's preferences, we found all of these trips for you!</strong>
+      <DropDownFilter options={locations} setter={setLocation} />
+    </span>
+  </div>
+);
+
+module.exports = GroupPreferencesBar;
+
