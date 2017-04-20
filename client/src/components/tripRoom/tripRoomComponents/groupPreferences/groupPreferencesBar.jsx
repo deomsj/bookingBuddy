3a33e20e72075bd67310557d8307277d88df52bc
Preston Moore <caroham29@gmail.com>
2017-04-20 16:41:11 -0700

    PreRevase

--- client/src/components/tripRoom/tripRoomComponents/groupPreferences/groupPreferencesBar.jsx
+++ client/src/components/tripRoom/tripRoomComponents/groupPreferences/groupPreferencesBar.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import DropDownFilter from './dropDownFilter.jsx';
+import {tripData, userData} from '../../../tripRoom/data/tripRoomDynamicData';
 /////////////////////////
 // Group Preferences Bar
 /////////////////////////
+class ShowBudget extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: ''
+    };
+  }
+
+  ComponentwillMount() {
+    console.log(tripData.profile, "tripData");
+  }
+
+}
+console.log(userData, "userData");
+
 
 var GroupPreferencesBar = ({priceRange, dateRange, locations, setLocation}) => (
   <div className="preferences-bar">
     <div className="row">
       <div className="col s3">
-        <span className="preferences-price"><i className="material-icons green-text">credit_card</i>{ priceRange }</span>
+        <span className="preferences-price"><i className="material-icons green-text">credit_card</i>priceRange </span>
       </div>
       <div className="col s3">
         <span className="preferences-date"><i className="material-icons green-text">today</i>{dateRange}</span>
