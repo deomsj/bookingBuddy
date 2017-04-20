d1986e3dc651db6c273e03a1bf07560d45a5bd3c
Nate Nault <nate@nathannault.com>
2017-04-20 23:34:40 +0800

    Adjust styling of trip room icons

--- client/src/components/tripRoom/tripRoomComponents/groupPreferences/groupPreferencesBar.jsx
+++ client/src/components/tripRoom/tripRoomComponents/groupPreferences/groupPreferencesBar.jsx
@@ -5,13 +5,13 @@ import DropDownFilter from './dropDownFilter.jsx';
 /////////////////////////
 
 var GroupPreferencesBar = ({priceRange, dateRange, locations, setLocation}) => (
-  <div>
+  <div className="preferences-bar">
     <div className="row">
       <div className="col s3">
-        <span><i className="material-icons green-text">credit_card</i>{ priceRange }</span><br />
+        <span className="preferences-price"><i className="material-icons green-text">credit_card</i>{ priceRange }</span>
       </div>
       <div className="col s3">
-        <span><i className="material-icons green-text">today</i>{dateRange}</span><br /><br />
+        <span className="preferences-date"><i className="material-icons green-text">today</i>{dateRange}</span>
       </div>
     </div>
     <span><strong>Based on your group's preferences, we found all of these trips for you!</strong>
