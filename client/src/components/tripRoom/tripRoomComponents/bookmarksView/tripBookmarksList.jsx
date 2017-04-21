763f1040db32f49f9e62a35d119e170f2fcd4eaf
Nate Nault <nate@nathannault.com>
2017-04-21 13:40:53 +0800

    Convert bookmark layout to materialize card component

--- client/src/components/tripRoom/tripRoomComponents/bookmarksView/tripBookmarksList.jsx
+++ client/src/components/tripRoom/tripRoomComponents/bookmarksView/tripBookmarksList.jsx
@@ -4,24 +4,24 @@ import BuddyVotesWindow from './BuddyVotesWindow.jsx';
 var TripBookmarksList = function({bookmarkedTrips, updateBookmarkVote}) {
 
   var bookmarksList = bookmarkedTrips.map( (bookmark, bookmarkId) => (
-    <div key={bookmarkId} className="row">
-      <div className="col s12 m6">
-        <h5>{bookmark.hotelRecomendationObj.HotelName}</h5>
-        <div className="col s12 m6">
-          <img src={bookmark.hotelRecomendationObj.Image} style={{'maxHeight':'300px', 'maxWidth':'100%'}} alt="picture"/>
-        </div>
-        <div className="col s12 m6">
+    <div key={bookmarkId} className="card">
+      <div className="card-image">
+        <img src={bookmark.hotelRecomendationObj.Image} alt="picture"/>
+      </div>
+      <div className="card-stacked">
+        <div className="card-content">
+          <h5>{bookmark.hotelRecomendationObj.HotelName}</h5>
           <p>{bookmark.bookmarkComment}</p>
         </div>
-      </div>
-      <div className="col s12 m6">
-        <BuddyVotesWindow bookmarkId={bookmarkId} buddyVotes={bookmark.buddyVotes} updateBookmarkVote={updateBookmarkVote}/>
+        <div className="card-action">
+          <BuddyVotesWindow bookmarkId={bookmarkId} buddyVotes={bookmark.buddyVotes} updateBookmarkVote={updateBookmarkVote}/>
+        </div>
       </div>
     </div>
   ));
 
   return  (
-    <div>
+    <div className="row">
       {bookmarksList}
     </div>
   );
