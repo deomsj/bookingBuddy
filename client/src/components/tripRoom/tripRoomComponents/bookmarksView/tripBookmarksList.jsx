36b2fe9226763b4201db2fb404c4445cb7290016
Nate Nault <nate@nathannault.com>
2017-04-22 23:45:43 +0800

    Include BookmarkMessages component within TripBookmrksList component

--- client/src/components/tripRoom/tripRoomComponents/bookmarksView/tripBookmarksList.jsx
+++ client/src/components/tripRoom/tripRoomComponents/bookmarksView/tripBookmarksList.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import BuddyVotesWindow from './BuddyVotesWindow.jsx';
+import BookmarkMessages from './BookmarkMessages.jsx';
 
 var TripBookmarksList = function({bookmarkedTrips, updateBookmarkVote}) {
 
   var bookmarksList = bookmarkedTrips.map( (bookmark, bookmarkId) => (
-    <div key={bookmarkId} className="card">
-      <div className="card-image">
-        <img src={bookmark.hotelRecomendationObj.Image} alt="picture"/>
-      </div>
-      <div className="card-stacked">
-        <div className="card-content">
+    <div key={bookmarkId} className="row">
+      <div className="col s12 m7">
+        <div className="col s12 m7">
           <h5>{bookmark.hotelRecomendationObj.HotelName}</h5>
           <p>{bookmark.bookmarkComment}</p>
         </div>
-        <div className="card-action">
-          <BuddyVotesWindow bookmarkId={bookmarkId} buddyVotes={bookmark.buddyVotes} updateBookmarkVote={updateBookmarkVote}/>
+        <div className="col s12 m5">
+          <img src={bookmark.hotelRecomendationObj.Image} style={{'maxHeight':'300px', 'maxWidth':'100%'}} alt="picture"/>
+        </div>
+      </div>
+      <div className="col s12 m5">
+        <BuddyVotesWindow
+          bookmarkId={bookmarkId}
+          buddyVotes={bookmark.buddyVotes}
+          updateBookmarkVote={updateBookmarkVote}
+        />
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <BookmarkMessages />
         </div>
       </div>
     </div>
