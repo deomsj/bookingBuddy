95e04f1cb94418f2fe8be74d887d2e44bae6ad3f
Jesse DeOms <jesse.deoms@gmail.com>
2017-04-25 16:03:32 -0700

    add loading view and consolidate trip room files

---
+++ client/src/components/tripRoom/tripRoomComponents/Loading/Loading.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Loading = () => (
+  <div>
+    <h3>Sit back and relax, we are searching all of the interwebs to find the best trips for your group</h3>
+    <div className="preloader-wrapper big active">
+      <div className="spinner-layer spinner-blue">
+        <div className="circle-clipper left">
+          <div className="circle"></div>
+        </div>
+        <div className="gap-patch">
+          <div className="circle"></div>
+        </div>
+        <div className="circle-clipper right">
+          <div className="circle"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Loading;
