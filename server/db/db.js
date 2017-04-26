92de1f509bf87bb827aa1bed8e54e17ecd394b44
Max Berger <maxamillianberger@gmail.com>
2017-04-26 10:53:02 -0700

    Add groupDates component, change email max length

--- server/db/db.js
+++ server/db/db.js
@@ -3,21 +3,23 @@ var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/test7';
 var db = new pg.Client(connectionString);
 
-db.connect(function (err) {
-  if (err) { throw err; }
+db.connect(function(err) {
+  if (err) {
+    throw err;
+  }
 });
 
 var tripCreatorTest = {
   //Once "create trip" button is hit, api call should be made...
-  email: 'johndoe@gmail.com',
+  email: 'johndoe@gmail.com'
   //backend will create random and unique trip id ex "abc123"
-    //Send that trip id back to db side as reference to that specific trip for all users
+  //Send that trip id back to db side as reference to that specific trip for all users
 };
 
 //3 IDEAL FRONTEND SUMBISSION FORMS BELOW!!!
 var tripMaster1 = {
   //tripMaster === Originator of trip
-    //John Doe creates trip
+  //John Doe creates trip
   email: 'johndoe@gmail.com',
   name: 'Doe',
   locations: ['New York', 'Atlanta', 'Pheonix'],
@@ -26,8 +28,8 @@ var tripMaster1 = {
   endDate: '10/16/2017',
   duration: '7',
   hotelBudget: 1100,
-  tripName : 'abc123',
-  tripSummary : "Test trip description"
+  tripName: 'abc123',
+  tripSummary: 'Test trip description'
 };
 
 var tripUser1 = {
@@ -41,7 +43,7 @@ var tripUser1 = {
   hotelBudget: 300,
   tripId: 1
   //CHANGE "tripId" TO WHATEVER TRIP ID IS CREATED FROM TRIP MASTER
-    //SELECT * FROM trips; <--- in postgres this will show your trip id
+  //SELECT * FROM trips; <--- in postgres this will show your trip id
 };
 
 var tripUser3 = {
@@ -56,30 +58,33 @@ var tripUser3 = {
   budget: 1200,
   tripId: 1
   //CHANGE "tripId" TO WHATEVER TRIP ID IS CREATED FROM TRIP MASTER
-    //SELECT * FROM trips; <--- in postgres this will show your trip id
+  //SELECT * FROM trips; <--- in postgres this will show your trip id
 };
-
-
 
 var tripMaster = function(obj) {
   console.log('Creating Trip Master!', obj);
-  db.query('INSERT INTO \
+  db.query(
+    'INSERT INTO \
                   trips(name, description) \
                   VALUES($1, $2) RETURNING id',
-                  [obj.tripName, obj.description], function(err, tripResults) {
-                    if (err) {
-                       console.log("Error in db.js on line 71:", err);
-                    }
+    [obj.tripName, obj.description],
+    function(err, tripResults) {
+      if (err) {
+        console.log('Error in db.js on line 71:', err);
+      }
 
-  db.query('INSERT INTO \
+      db.query(
+        'INSERT INTO \
                   users(namef, namel, email) \
                   VALUES($1, $2, $3) RETURNING id',
-                  [obj.name, obj.name, obj.email], function(err, userResults) {
-                    console.log('userResults:', userResults);
-                    if (err) {
-                      console.error('Error in db.js on line 81:', err)
-                    }
+        [obj.name, obj.name, obj.email],
+        function(err, userResults) {
+          console.log('userResults:', userResults);
+          if (err) {
+            console.error('Error in db.js on line 81:', err);
+          }
 
+<<<<<<< HEAD
       db.query('INSERT INTO \
                       userTrips(user_id, trip_id) \
                       VALUES($1, $2) RETURNING id',
@@ -120,154 +125,249 @@ var tripMaster = function(obj) {
               });
             });
           });
-};
-
-  // console.log(obj.buddies, "BUDDIES!")
-  // obj.buddies.forEach(function(item, ind, coll) {
-  //   console.log(item, "ITEM");
-  //   db.query('INSERT INTO \
-  //                 users(namef, namel, email) \
-  //                 VALUES($1, $2, $3) RETURNING id',
-  //                 [item.name, item.name, item.email], function(err, userResults) {
-  //                   // console.log(userResults, "userResults")
-  //                   if (err) {
-  //                     // res.send(err)
-  //                   }
-  //     db.query('INSERT INTO \
-  //                   trips(name, description) \
-  //                   VALUES($1, $2) RETURNING id',
-  //                   [obj.tripName, obj.description], function(err, tripResults) {
-  //                     if (err) {
-  //                     // console.log(tripResults);
-  //                     // res.send(err)
-  //                     }
-  //       db.query('INSERT INTO \
-  //                     userTrips(user_id, trip_id) \
-  //                     VALUES($1, $2) RETURNING id',
-  //                     [userResults.rows[0].id, tripResults.rows[0].id], function(err, userTripsResults) {
-  //                       if (err) {
-  //                         // res.send(err)
-  //                       }
-  //       });
-  //     });
-  //   });
-  // });
-
-var moreTrips = function(obj) {
-  console.log('Creating Additional Trips', obj);
-
-  db.query('INSERT INTO \
-                  trips(name, description) \
-                  VALUES($1, $2) RETURNING id',
-                  [obj.tripName, obj.description], function(err, tripResults) {
-                    if (err) {
-                      // console.log(tripResults);
-                      // res.send(err)
-                    }
-
-  db.query('INSERT INTO \
+=======
+          db.query(
+            'INSERT INTO \
                   userTrips(user_id, trip_id) \
                   VALUES($1, $2) RETURNING id',
-                  [obj.id, tripResults.rows[0].id], function(err, userTripsResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
-
-                    // console.log(userTripsResults.rows[0], "HELLO!");
-  db.query('INSERT INTO \
+            [userResults.rows[0].id, tripResults.rows[0].id],
+            function(err, userTripsResults) {
+              if (err) {
+                // res.send(err)
+              }
+              // console.log(userTripsResults.rows[0], "HELLO!");
+              db.query(
+                'INSERT INTO \
                   dates(beging, ending, duration, trip_id, trip_number) \
                   VALUES($1, $2, $3, $4, $5) RETURNING id',
-                  [obj.beginDate.slice(5)+'/'+obj.beginDate.slice(0,4), obj.endDate.slice(5)+'/'+obj.endDate.slice(0,4), obj.duration, userTripsResults.rows[0].id, tripResults.rows[0].id], function(err, dateResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+                [
+                  obj.beginDate.slice(5) + '/' + obj.beginDate.slice(0, 4),
+                  obj.endDate.slice(5) + '/' + obj.endDate.slice(0, 4),
+                  obj.duration,
+                  userTripsResults.rows[0].id,
+                  tripResults.rows[0].id
+                ],
+                function(err, dateResults) {
+                  if (err) {
+                    // res.send(err)
+                  }
 
-  db.query('INSERT INTO \
-                  budget(total, trip_id) \
-                  VALUES($1, $2) RETURNING id',
-                  [obj.hotelBudget, userTripsResults.rows[0].id], function(err, budgetResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+                  db.query(
+                    'INSERT INTO \
+                  budget(total, trip_id, flight, activitites) \
+                  VALUES($1, $2, $3, $4) RETURNING id',
+                    [
+                      obj.hotelBudget,
+                      userTripsResults.rows[0].id,
+                      obj.flightBudget,
+                      obj.activitiesBudget
+                    ],
+                    function(err, budgetResults) {
+                      if (err) {
+                        // res.send(err)
+                      }
 
-  obj.locations.forEach(function(location, ind, coll) {
-  db.query('INSERT INTO \
+                      obj.locations.forEach(function(location, ind, coll) {
+                        db.query(
+                          'INSERT INTO \
                   locations(name, user_trip_id, trip_id) \
                   VALUES($1, $2, $3) RETURNING id',
-                  [location, userTripsResults.rows[0].id, tripResults.rows[0].id], function(err, userResults) {
-                    if (err) {
-                      // res.send(err)
+                          [location, userTripsResults.rows[0].id, tripResults.rows[0].id],
+                          function(err, userResults) {
+                            if (err) {
+                              // res.send(err)
+                            }
+                          }
+                        );
+                      });
                     }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+>>>>>>> Add groupDates component, change email max length
+};
+
+// console.log(obj.buddies, "BUDDIES!")
+// obj.buddies.forEach(function(item, ind, coll) {
+//   console.log(item, "ITEM");
+//   db.query('INSERT INTO \
+//                 users(namef, namel, email) \
+//                 VALUES($1, $2, $3) RETURNING id',
+//                 [item.name, item.name, item.email], function(err, userResults) {
+//                   // console.log(userResults, "userResults")
+//                   if (err) {
+//                     // res.send(err)
+//                   }
+//     db.query('INSERT INTO \
+//                   trips(name, description) \
+//                   VALUES($1, $2) RETURNING id',
+//                   [obj.tripName, obj.description], function(err, tripResults) {
+//                     if (err) {
+//                     // console.log(tripResults);
+//                     // res.send(err)
+//                     }
+//       db.query('INSERT INTO \
+//                     userTrips(user_id, trip_id) \
+//                     VALUES($1, $2) RETURNING id',
+//                     [userResults.rows[0].id, tripResults.rows[0].id], function(err, userTripsResults) {
+//                       if (err) {
+//                         // res.send(err)
+//                       }
+//       });
+//     });
+//   });
+// });
+
+var moreTrips = function(obj) {
+  console.log('Creating Trip Master!', obj);
+  db.query(
+    'INSERT INTO \
+                  trips(name, description) \
+                  VALUES($1, $2) RETURNING id',
+    [obj.tripName, obj.description],
+    function(err, tripResults) {
+      if (err) {
+        // console.log(tripResults);
+        // res.send(err)
+      }
+
+      db.query(
+        'INSERT INTO \
+                  userTrips(user_id, trip_id) \
+                  VALUES($1, $2) RETURNING id',
+        [obj.id, tripResults.rows[0].id],
+        function(err, userTripsResults) {
+          if (err) {
+            // res.send(err)
+          }
+          // console.log(userTripsResults.rows[0], "HELLO!");
+          db.query(
+            'INSERT INTO \
+                  dates(beging, ending, duration, trip_id, trip_number) \
+                  VALUES($1, $2, $3, $4, $5) RETURNING id',
+            [
+              obj.beginDate.slice(5) + '/' + obj.beginDate.slice(0, 4),
+              obj.endDate.slice(5) + '/' + obj.endDate.slice(0, 4),
+              obj.duration,
+              userTripsResults.rows[0].id,
+              tripResults.rows[0].id
+            ],
+            function(err, dateResults) {
+              if (err) {
+                // res.send(err)
+              }
+
+              db.query(
+                'INSERT INTO \
+                  budget(total, trip_id) \
+                  VALUES($1, $2) RETURNING id',
+                [obj.hotelBudget, userTripsResults.rows[0].id],
+                function(err, budgetResults) {
+                  if (err) {
+                    // res.send(err)
+                  }
+
+                  obj.locations.forEach(function(location, ind, coll) {
+                    db.query(
+                      'INSERT INTO \
+                  locations(name, user_trip_id) \
+                  VALUES($1, $2) RETURNING id',
+                      [location, userTripsResults.rows[0].id],
+                      function(err, userResults) {
+                        if (err) {
+                          // res.send(err)
+                        }
+                      }
+                    );
                   });
-                });
-              });
-            });
-          });
-        });
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 };
 
 var tripUser = function(obj) {
   console.log('Creating Trip User!');
 
-  db.query('INSERT INTO \
+  db.query(
+    'INSERT INTO \
                   users(namef, namel, email) \
                   VALUES($1, $2, $3) RETURNING id',
-                  [obj.name, obj.name, obj.email], function(err, userResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+    [obj.name, obj.name, obj.email],
+    function(err, userResults) {
+      if (err) {
+        // res.send(err)
+      }
 
-  db.query('INSERT INTO \
+      db.query(
+        'INSERT INTO \
                   userTrips(user_id, trip_id) \
                   VALUES($1, $2) RETURNING id, trip_id',
-                  [userResults.rows[0].id, obj.tripId], function(err, userTripsResults) {
-                    if (err) {
-// IF YOU EXPERIENCE ERROR HERE CHANGE "tripId" in tripUser object TO WHATEVER TRIP ID IS CREATED FROM TRIP MASTER
-  //SELECT * FROM trips; <--- in postgres, this will show your trip id
-                      // res.send(err)
-                    }
+        [userResults.rows[0].id, obj.tripId],
+        function(err, userTripsResults) {
+          if (err) {
+            // IF YOU EXPERIENCE ERROR HERE CHANGE "tripId" in tripUser object TO WHATEVER TRIP ID IS CREATED FROM TRIP MASTER
+            //SELECT * FROM trips; <--- in postgres, this will show your trip id
+            // res.send(err)
+          }
 
-  db.query('INSERT INTO \
+          db.query(
+            'INSERT INTO \
                   dates(beging, ending, duration, trip_id, trip_number) \
                   VALUES($1, $2, $3, $4, $5) RETURNING id',
-                  [obj.beginDate, obj.endDate, obj.duration, userTripsResults.rows[0].id, obj.tripId ], function(err, dateResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+            [obj.beginDate, obj.endDate, obj.duration, userTripsResults.rows[0].id, obj.tripId],
+            function(err, dateResults) {
+              if (err) {
+                // res.send(err)
+              }
 
-  db.query('INSERT INTO \
+              db.query(
+                'INSERT INTO \
                   budget(total, trip_id) \
                   VALUES($1, $2) RETURNING id',
-                  [obj.hotelBudget, userTripsResults.rows[0].id], function(err, budgetResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+                [obj.hotelBudget, userTripsResults.rows[0].id],
+                function(err, budgetResults) {
+                  if (err) {
+                    // res.send(err)
+                  }
 
-  obj.locations.forEach(function(location, ind, coll) {
-  db.query('INSERT INTO \
+                  obj.locations.forEach(function(location, ind, coll) {
+                    db.query(
+                      'INSERT INTO \
                   locations(name, user_trip_id, trip_id) \
                   VALUES($1, $2, $3) RETURNING id',
-                  [location, userTripsResults.rows[0].id, userTripsResults.rows[0].trip_id],
-                  function(err, userResults) {
-                    if (err) {
-                      // res.send(err)
-                    }
+                      [location, userTripsResults.rows[0].id, userTripsResults.rows[0].trip_id],
+                      function(err, userResults) {
+                        if (err) {
+                          // res.send(err)
+                        }
+                      }
+                    );
                   });
-          });
-        });
-      });
-    });
-  });
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 };
 // tripMaster(tripMaster1);
 // tripUser(tripUser1);
 
-
-
-
-
 module.exports = {
-  db : db,
-  tripMaster : tripMaster,
-  moreTrips : moreTrips
-}
+  db: db,
+  tripMaster: tripMaster,
+  moreTrips: moreTrips
+};
--- server/db/seed.js
+++ server/db/seed.js
@@ -4,65 +4,81 @@ var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/test7';
 var db = new pg.Client(connectionString);
 
-db.connect(function (err) {
+db.connect(function(err) {
   // uncomment and run node seed.js to create db.
-    // Nothing changes except instead of "test1" above you must insert your own db name on your local computer or create database
+  // Nothing changes except instead of "test1" above you must insert your own db name on your local computer or create database
 
-  if (err) { throw err; }
+  if (err) {
+    throw err;
+  }
 
-  db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   users( \
                   id SERIAL PRIMARY KEY, \
                   nameF VARCHAR(50) not null, \
                   nameL VARCHAR(50) not null, \
                   email VARCHAR(50) not null)');
 
-  db.query('CREATE TABLE \
+
+  db.query(
+    'CREATE TABLE \
                   trips( \
                   id SERIAL PRIMARY KEY, \
                   description VARCHAR(300), \
                   name VARCHAR(100) not null)');
 
-  db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   userTrips( \
                   id SERIAL PRIMARY KEY, \
                   user_id INTEGER REFERENCES users(id), \
-                  trip_id INTEGER REFERENCES trips(id))');
+                  trip_id INTEGER REFERENCES trips(id))'
+  );
 
-db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   budget( \
                   id SERIAL PRIMARY KEY, \
                   total NUMERIC(6,2) not null, \
                   flight NUMERIC(6,2), \
                   activitites NUMERIC(6,2), \
-                  trip_id INTEGER REFERENCES userTrips(id))');
+                  trip_id INTEGER REFERENCES userTrips(id))'
+  );
 
-db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   dates( \
                   id SERIAL PRIMARY KEY, \
                   beging VARCHAR(15) not null, \
                   ending VARCHAR(15) not null, \
                   duration VARCHAR(10) not null, \
                   trip_id INTEGER REFERENCES userTrips(id), \
-                  trip_number INTEGER REFERENCES trips(id))');
+                  trip_number INTEGER REFERENCES trips(id))'
+  );
 
-db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   locations( \
                   id SERIAL PRIMARY KEY, \
                   name VARCHAR(50) not null, \
                   trip_id INTEGER REFERENCES trips(id), \
-                  user_trip_id INTEGER REFERENCES userTrips(id))');
+                  user_trip_id INTEGER REFERENCES userTrips(id))'
+  );
 
-  db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   bookmarks( \
                   id SERIAL PRIMARY KEY, \
                   bookmark_id VARCHAR(26), \
                   name VARCHAR(50), \
                   hotel_id VARCHAR(50), \
                   bookmark VARCHAR(300), \
-                  trip_id INTEGER REFERENCES trips(id))');
+                  trip_id INTEGER REFERENCES trips(id))'
+  );
 
-  db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   comments( \
                   id SERIAL PRIMARY KEY, \
                   client_id VARCHAR(26), \
@@ -70,17 +86,17 @@ db.query('CREATE TABLE \
                   email VARCHAR(50), \
                   comment VARCHAR(300), \
                   trip_id INTEGER REFERENCES trips(id), \
-                  bookmark_id VARCHAR)');
+                  bookmark_id VARCHAR)'
+  );
 
-  db.query('CREATE TABLE \
+  db.query(
+    'CREATE TABLE \
                   votes( \
                   id SERIAL PRIMARY KEY, \
                   name VARCHAR(50), \
                   email VARCHAR(50), \
                   vote VARCHAR(10), \
                   trip_id INTEGER REFERENCES trips(id), \
-                  bookmark_id VARCHAR(22))');
-
-
+                  bookmark_id VARCHAR(22))'
+  );
 });
-
