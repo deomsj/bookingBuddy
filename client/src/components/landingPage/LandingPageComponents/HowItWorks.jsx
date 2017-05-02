2d94c80043d3c63b2409b379ca8a1f4367abd5b8
Jesse DeOms <jesse.deoms@gmail.com>
2017-05-02 08:08:39 -0700

    Edits to text descriptions on landing page

--- client/src/components/landingPage/LandingPageComponents/HowItWorks.jsx
+++ client/src/components/landingPage/LandingPageComponents/HowItWorks.jsx
@@ -14,7 +14,7 @@ var HowItWorksSection = function() {
             <div className="icon-block">
               <h2 className="center green-text darken-2"><i className="material-icons">location_on</i></h2>
               <h5 className="center">Location</h5>
-              <p>Choose one, or several locations where you'd like to travel. If you're friends have already chosen some locations, you can also choose from theirs. You can add to this list at any time. After that, everyone can vote on their favorites.</p>
+              <p>Choose one or several destinations that you'd like to travel to. View your buddies' selections as you fill in your own. Agree with their ideas and throw out new ideas as well! Everyone can vote on their favorites and continue to add to this list at any time.</p>
             </div>
           </div>
           {/* BLOCK 2 */}
@@ -22,7 +22,7 @@ var HowItWorksSection = function() {
             <div className="icon-block">
               <h2 className="center green-text darken-2"><i className="material-icons">today</i></h2>
               <h5 className="center">Date</h5>
-              <p>Which dates are you available? When you and your buddies pick dates, the whole group can see them in order to figure out what works best for everyone. Are your dates not working out? Just go back and modify them at any time.</p>
+              <p>Which dates are you available? When you and your buddies pick dates, the whole group can see them in order to figure out what works best for everyone. Are your dates not working out? We make it easy to sync up your calendars so you can start enjoying the fun parts of planning your vacation!</p>
             </div>
           </div>
           {/* BLOCK 3 */}
@@ -30,7 +30,7 @@ var HowItWorksSection = function() {
             <div className="icon-block">
               <h2 className="center bling-header green-text darken-2">$</h2>
               <h5 className="center">Budget</h5>
-              <p>Almost everyone has a budget. We promise not to make it awkward for you. Let us know how much you can spend and we'll find trip options that work for everyone, without breaking the bank.</p>
+              <p>We're all on a budget... but we're not always on the same budget. We'll take those awkward moments out of traveling with friends. Let us know how much you can spend and we'll find trip options that work for everyone, without breaking the bank.</p>
             </div>
           </div>
         </div>
