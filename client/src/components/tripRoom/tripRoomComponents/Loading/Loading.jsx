c8c276f7134e4f22f4dba0b96062e6900a5d2352
Nate Nault <nate@nathannault.com>
2017-05-04 12:08:58 +0800

    Style loading page content

--- client/src/components/tripRoom/tripRoomComponents/Loading/Loading.jsx
+++ client/src/components/tripRoom/tripRoomComponents/Loading/Loading.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 const Loading = () => (
   <div className="container">
     <div className="row">
-      <h3>Sit back and relax, we are searching all of the interwebs to find the best trips for your group</h3>
-      <div className="preloader-wrapper big active">
-        <div className="spinner-layer spinner-blue">
-          <div className="circle-clipper left">
-            <div className="circle"></div>
-          </div>
-          <div className="gap-patch">
-            <div className="circle"></div>
-          </div>
-          <div className="circle-clipper right">
-            <div className="circle"></div>
+      <div className="loading-card card-panel grey lighten-4">
+        <h3>Sit back and relax, we are searching all of the interwebs to find the best trips for your group</h3>
+        <div className="preloader-wrapper big active">
+          <div className="spinner-layer spinner-blue">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div>
+            <div className="gap-patch">
+              <div className="circle"></div>
+            </div>
+            <div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
           </div>
         </div>
       </div>
